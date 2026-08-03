84c0a9685e57d5363947150e35f0d3e954e43501
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-03 15:09:46 +0530

    prog3

---
+++ prog3.c
@@ -0,0 +1,23 @@
+//write a c program to sum of two number.
+
+#include<stdio.h>
+
+int main(){
+
+    int a;
+    int b;
+    int sum;
+    
+    printf("enter your first number:");
+    scanf("%d",&a);
+
+    printf("enter your second number:");
+    scanf("%d",&b);
+
+    sum =a+b;
+
+    printf("your sum is: %d",sum);
+
+    
+    return 0;
+}
