aa5e50d7b1d9eabcbb8dc8a0b34b859e65d56d2d
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-03 15:57:50 +0530

    prog11

---
+++ progt11.c
@@ -0,0 +1,22 @@
+//check even and odd
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter Number: ");
+    scanf("%d",&num);
+
+    if(num % 2 == 0)
+    {
+        printf("Even Number");
+    }
+    else
+    {
+        printf("Odd Number");
+    }
+
+    return 0;
+}
