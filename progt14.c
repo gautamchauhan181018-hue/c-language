bf020c4843bdad78b375f002d00ec6227969884b
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-03 16:07:08 +0530

    prog14

---
+++ progt14.c
@@ -0,0 +1,22 @@
+// Greatest of Two Numbers
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+
+    printf("enter the value of a and b: ");
+    scanf("%d%d",&a,&b);
+
+    if(a > b)
+    {
+        printf("%d is Greater",a);
+    }
+    else
+    {
+        printf("%d is Greater",b);
+    }
+
+    return 0;
+}
