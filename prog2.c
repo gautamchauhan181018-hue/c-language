82fad0225999b9829d281878b6248383c600670b
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-03 14:52:27 +0530

    prog2

--- prog2.c
+++ prog2.c
@@ -1,1 +1,26 @@
-//
+//2 - create a program to use format specifier.
+
+#include<stdio.h>
+
+int main(){
+
+    int a;
+    
+    float c;
+
+    printf("enter the roll:");
+    scanf(" %d",&a);
+
+    
+    printf("enter the decimal :");
+    scanf(" %f",&c);
+
+    printf("\nYour Information:\n");
+    printf("Roll = %d\n", a);
+    printf("Decimal = %.2f\n", c);
+
+
+
+
+    return 0;
+}
