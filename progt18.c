e2a86f996bbc192a3295542045064ba4e3960c01
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-03 16:33:02 +0530

    prog18

--- progt18.c
+++ progt18.c
@@ -3,6 +3,19 @@
 #include<stdio.h>
 
 int main(){
-    
+    int a,b,c;
+
+    printf("enter your three number:");
+    scanf("%d%d%d",&a,&b,&c);
+
+    if(a<b && a<c){
+        printf("a is smallest");
+    }
+    else if(b<a && b<c){
+        printf("b is smallest");
+    }
+    else{
+        printf("c is smallest");
+    }
     return 0;
 }
