2676f37b197726d4cb1e93267dfed041f6ba8784
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-03 15:23:46 +0530

    prog6

---
+++ pro6.c
@@ -0,0 +1,15 @@
+//take a character from user and print
+
+#include<stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    printf("You entered: %c", ch);
+
+    return 0;
+}
