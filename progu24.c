1ec47155e880fd4fb9b0812f327f1063d5882b17
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-04 23:35:30 +0530

    prog24

---
+++ progu24.c
@@ -0,0 +1,20 @@
+//sum of first n numbers
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0;
+
+    printf("Enter Number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
