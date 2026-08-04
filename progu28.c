bd6e49c77f6c1489ae9903e9aa9e71a135d556e0
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-04 23:47:23 +0530

    progu28.c

---
+++ progu28.c
@@ -0,0 +1,22 @@
+//reverse number
+
+#include<stdio.h>
+
+int main()
+{
+    int num, rev = 0, rem;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    while(num != 0)
+    {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num = num / 10;
+    }
+
+    printf("Reverse = %d", rev);
+
+    return 0;
+}
