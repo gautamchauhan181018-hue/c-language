db394cc4e3d856125454bc933991129042a8004a
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-04 23:38:08 +0530

    prog25

---
+++ progu25.c
@@ -0,0 +1,20 @@
+//factorial program
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    long long fact = 1;
+
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    printf("%lld", fact);
+
+    return 0;
+}
