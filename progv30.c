169179908d689f8425f529b31155ca1b4425fce4
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-04 23:51:04 +0530

    prog30

---
+++ progv30.c
@@ -0,0 +1,27 @@
+//Palindrome Number
+
+#include<stdio.h>
+
+int main()
+{
+    int num, temp, rev = 0, rem;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while(num != 0)
+    {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num = num / 10;
+    }
+
+    if(temp == rev)
+        printf("Palindrome Number");
+    else
+        printf("Not Palindrome Number");
+
+    return 0;
+}
