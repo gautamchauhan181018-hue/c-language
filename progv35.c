1b3fb6aa75e84b7dd42cacb644352cc17d31e714
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-08 10:35:06 +0530

    prog35

---
+++ progv35.c
@@ -0,0 +1,24 @@
+//print the sum of array 
+
+#include<stdio.h>
+
+int main(){
+
+int num[5];
+int sum = 0;
+
+printf("enter the 5 element of array: ");
+
+for(int i=0;i<5;i++){
+
+scanf("%d", &num[i]);
+sum = sum + arr[i];
+
+}
+
+printf("sum = %d", sum);
+
+return 0;
+
+}
+
