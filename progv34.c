0c2a853856e441771c5eed943398d0beb6249355
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-08 10:13:14 +0530

    prog34

--- progv34.c
+++ progv34.c
@@ -1,9 +1,24 @@
+//write a program of simple array.
+
 #include <stdio.h>
 
 int main(){
 
+
+     int num[5];
+
+
      for(int i=0;i<5;i++){
-    print
+
+    printf("enter the element of array:");
+    scanf("%d",&num[i]);
+
      }
+
+   for(int i = 0;i<5;i++){
+
+    printf("your array elements are: %d ",num[i]);
+
+   }
 
 }
